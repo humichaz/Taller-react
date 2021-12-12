3750f21f7c49b0e74ab201b40d179df0917de616
humichaz <69659922+humichaz@users.noreply.github.com>
2021-12-12 10:22:34 -0500

    testing 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ export default function App() {
   return (
     <div className="App">
       <Button variant="contained">Hello World</Button>
+
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
